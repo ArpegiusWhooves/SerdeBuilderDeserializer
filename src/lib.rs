bbc72259e05a4247300778f2da8f97f6c364a915
Arpegius Whooves <arpegius.whooves@gmail.com>
2024-03-02 21:33:11 +0100

    WIP: add BuilderMapAccess

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::borrow::Cow;
 use std::fmt::Display;
-use serde::Deserialize;
+use serde::{forward_to_deserialize_any, Deserialize};
 use serde::de::{
     self, DeserializeSeed, EnumAccess, IntoDeserializer, MapAccess, SeqAccess,
     VariantAccess, Visitor, Error
@@ -11,13 +11,14 @@ pub enum BuilderDataType<'de> {
     Boolean(bool),
     Integer(i64),
     Number(f64),
-    Map( Vec<(Cow<'de, str>,BuilderDataType<'de>)> ),
     String(Cow<'de, str>),
+    Map( Vec<(BuilderDataType<'de>,BuilderDataType<'de>)> ),
+    List( Vec<BuilderDataType<'de>> ),
 }
 
 #[derive(Debug)]
 pub enum BuilderError {
-
+    InvalidMapAccess
 }
 
 impl std::fmt::Display for BuilderError {
@@ -26,8 +27,7 @@ impl std::fmt::Display for BuilderError {
     }
 }
 
-impl std::error::Error for BuilderError {
-    
+impl std::error::Error for BuilderError { 
 }
 
 impl Error for BuilderError {
@@ -35,231 +35,104 @@ impl Error for BuilderError {
         todo!()
     }
 }
-
-
-pub struct BuilderDeserializer<'de>(&'de BuilderDataType<'de>);
+pub struct BuilderDeserializer<'de>(BuilderDataType<'de>);
 
 impl<'de> BuilderDeserializer<'de> {
-    pub fn from_data(input: &'de BuilderDataType) -> Self {
+    pub fn from_data(input: BuilderDataType<'de>) -> Self {
         BuilderDeserializer(input)
     }
 }
 
-impl<'de, 'a> serde::de::Deserializer<'de> for &'a mut BuilderDeserializer<'de> {
+struct BuilderMapAccess<'de> {
+    data: Vec<(BuilderDataType<'de>,BuilderDataType<'de>)>,
+    leftover: Option<BuilderDataType<'de>>,
+}
+
+impl<'de, 'a> serde::Deserializer<'de> for BuilderDeserializer<'de> {
     type Error = BuilderError;
 
     fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de> {
         match self.0 {
-            BuilderDataType::Boolean(v) => todo!(),
-            BuilderDataType::Integer(v) => todo!(),
-            BuilderDataType::Number(v) => todo!(),
-            BuilderDataType::Map(v) => todo!(),
-            BuilderDataType::String(v) => todo!(),
+            BuilderDataType::Boolean(v) => visitor.visit_bool(v),
+            BuilderDataType::Integer(v) => visitor.visit_i64(v),
+            BuilderDataType::Number(v) => visitor.visit_f64(v),
+            BuilderDataType::String(c) => match c {
+                Cow::Borrowed(v) => visitor.visit_borrowed_str(v),
+                Cow::Owned(v) => visitor.visit_string(v),
+            },
+            BuilderDataType::Map(v) => visitor.visit_map(BuilderMapAccess{
+                data: v,
+                leftover: None
+            }),
+            BuilderDataType::List(v) => todo!(),
         }
     }
-
-    fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-            todo!()
-    }
-
-    fn deserialize_i8<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_i16<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_i32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_u8<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_u16<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_u32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_f32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_f64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_char<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_string<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_byte_buf<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_option<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_unit<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_unit_struct<V>(
-        self,
-        name: &'static str,
-        visitor: V,
-    ) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_newtype_struct<V>(
-        self,
-        name: &'static str,
-        visitor: V,
-    ) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_tuple<V>(self, len: usize, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_tuple_struct<V>(
-        self,
-        name: &'static str,
-        len: usize,
-        visitor: V,
-    ) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_map<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_struct<V>(
-        self,
-        name: &'static str,
-        fields: &'static [&'static str],
-        visitor: V,
-    ) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_enum<V>(
-        self,
-        name: &'static str,
-        variants: &'static [&'static str],
-        visitor: V,
-    ) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_identifier<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
-    }
-
-    fn deserialize_ignored_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de> {
-        todo!()
+    forward_to_deserialize_any! {
+        bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char str string
+        bytes byte_buf option unit unit_struct newtype_struct seq tuple
+        tuple_struct map struct enum identifier ignored_any
     }
 }
 
 
-pub fn from_data<'a, T>(data: &'a BuilderDataType<'a>) -> Result<T,BuilderError>
+impl<'de> MapAccess<'de> for BuilderMapAccess<'de> {
+    type Error = BuilderError;
+
+    fn next_key_seed<K>(&mut self, seed: K) -> Result<Option<K::Value>, Self::Error>
+    where
+        K: DeserializeSeed<'de> {
+        if let Some((a,b)) = self.data.pop() {
+            self.leftover = Some(b); 
+            let v = seed.deserialize(BuilderDeserializer::from_data(a))?;
+            Ok(Some(v))
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value, Self::Error>
+    where
+        V: DeserializeSeed<'de> {
+        if let Some(leftover) = self.leftover.take() {
+            seed.deserialize(BuilderDeserializer::from_data(leftover))
+        } else {
+            Err(BuilderError::InvalidMapAccess)
+        }
+    }
+    
+    fn size_hint(&self) -> Option<usize> {
+        Some(self.data.len())
+    }
+    
+    fn next_entry_seed<K, V>(
+        &mut self,
+        kseed: K,
+        vseed: V,
+    ) -> Result<Option<(K::Value, V::Value)>, Self::Error>
+    where
+        K: DeserializeSeed<'de>,
+        V: DeserializeSeed<'de>,
+    {
+        if let Some((a,b)) = self.data.pop() {
+            self.leftover = None;
+            let va  = kseed.deserialize(BuilderDeserializer::from_data(a))?;
+            let vb  = vseed.deserialize(BuilderDeserializer::from_data(b))?;
+            Ok(Some((va,vb)))
+        } else {
+            Ok(None)
+        }
+    }
+    
+}
+
+
+pub fn from_data<'a, T>(data: BuilderDataType<'a>) -> Result<T,BuilderError>
 where
     T: Deserialize<'a>,
-{
-    let mut deserializer = BuilderDeserializer::from_data(data);
-    let t = T::deserialize(&mut deserializer)?;
-    Ok(t)
+{ 
+    Ok(T::deserialize(BuilderDeserializer::from_data(data))?)
 }
 
 #[cfg(test)]
@@ -273,7 +146,7 @@ mod tests {
         b: bool,
         c: String
     }
-
+    
     #[test]
     fn it_works() {
 
@@ -287,12 +160,12 @@ mod tests {
         println!("{:?}",&result);
 
         let data = BuilderDataType::Map(vec![
-            (Cow::from("a"),BuilderDataType::Integer(123)),
-            (Cow::from("b"),BuilderDataType::Boolean(true)),
-            (Cow::from("c"),BuilderDataType::String(Cow::from("test"))),
+            (BuilderDataType::String(Cow::from("a")),BuilderDataType::Integer(123)),
+            (BuilderDataType::String(Cow::from("b")),BuilderDataType::Boolean(true)),
+            (BuilderDataType::String(Cow::from("c")),BuilderDataType::String(Cow::from("test"))),
         ]);
 
-        let test: Test = from_data(&data).unwrap();
+        let test: Test = from_data(data).unwrap();
 
         assert_eq!(result, test);
     }
